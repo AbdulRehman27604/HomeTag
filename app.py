83a69497ae18e8dd63d410aec5b5757fd3a962c3
Abdul Rehman <abdulz@Abduls-MacBook-Air.local>
2025-10-06 21:05:48 -0700

    Describe what you changed here

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Stack2764@localhost/Tracker'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Stack2764@localhost/Tracker'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hometag_db_user:hChQAQXkhGsuGxCiZgEOjruFYqF7dBGf@dpg-d3i8vrripnbc73dvqqjg-a/hometag_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
