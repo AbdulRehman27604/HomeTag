348bfdb56fcf51fc6c74fb31358735788d0e0d72
Abdul Rehman <abdulz@Abduls-MacBook-Air.local>
2025-10-06 21:47:12 -0700

    Updated app.py with Render DB configuration

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Stack2764@localhost/Tracker'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hometag_db_user:hChQAQXkhGsuGxCiZgEOjruFYqF7dBGf@dpg-d3i8vrripnbc73dvqqjg-a/hometag_db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hometag_db_user:hChQAQXkhGsuGxCiZgEOjruFYqF7dBGf@dpg-d3i8vrripnbc73dvqqjg-a.oregon-postgres.render.com/hometag_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
