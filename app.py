072e76400b6263bdd9b9a4b9941a3f575e08d300
Abdul Rehman <abdulz@Abduls-MacBook-Air.local>
2025-10-10 21:24:56 -0700

    changes

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
 app.secret_key = "dfsfscasfvsrvsvasavawev"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Stack2764@localhost/Tracker'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Stack2764@localhost/Tracker'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hometag_db_user:hChQAQXkhGsuGxCiZgEOjruFYqF7dBGf@dpg-d3i8vrripnbc73dvqqjg-a/hometag_db'
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hometag_db_user:hChQAQXkhGsuGxCiZgEOjruFYqF7dBGf@dpg-d3i8vrripnbc73dvqqjg-a.oregon-postgres.render.com/hometag_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hometag_db_user:hChQAQXkhGsuGxCiZgEOjruFYqF7dBGf@dpg-d3i8vrripnbc73dvqqjg-a.oregon-postgres.render.com/hometag_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
